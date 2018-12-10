e1bf21736a0ba78259fcda54f2bd445631581dca
Maarten de Klerk <maarten.deklerk@gmail.com>
2018-12-10 12:47:08 +0100

    Added Spring Initializr project

---
+++ server/src/test/java/ai/openbanking/OpenBanking/OpenBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package ai.openbanking.OpenBanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenBankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
